a0df329685ff521558267347bd484c05f62194cf
ChronosX88 <chronosx88@gmail.com>
2019-10-24 18:41:13 +0400

    feat: [Repositories] Add RoomRepository and implement creating entry in database (also deleting it later)

---
+++ internal/repositories/roomRepository.go
@@ -0,0 +1,46 @@
+package repositories
+
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+
+	"github.com/MoonSHRD/sonis/internal/database"
+	"github.com/MoonSHRD/sonis/internal/models"
+	"github.com/prprprus/scheduler"
+)
+
+type RoomRepository struct {
+	db                    *database.Database
+	deletingRoomScheduler *scheduler.Scheduler
+	logger                *logrus.Logger
+}
+
+func NewRoomRepository(db *database.Database) (*RoomRepository, error) {
+	if db != nil {
+		deletingRoomScheduler, err := scheduler.NewScheduler(10000)
+		if err != nil {
+			return nil, err
+		}
+		return &RoomRepository{
+			db:                    db,
+			deletingRoomScheduler: deletingRoomScheduler,
+			logger:                logrus.New(),
+		}, nil
+	}
+	return nil, fmt.Errorf("database connection is null")
+}
+
+func (rr *RoomRepository) CreateRoom(room models.Room) error {
+	err := rr.db.GetDatabaseConnection().Insert(&room)
+	if err != nil {
+		return err
+	}
+	rr.deletingRoomScheduler.Delay().Second(room.TTL).Do(func() {
+		err := rr.db.GetDatabaseConnection().Delete(&room)
+		if err != nil {
+			rr.logger.Error("Cannot delete room " + room.RoomID + ". Reason: " + err.Error())
+		}
+	})
+	return nil
+}
