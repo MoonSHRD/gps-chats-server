6c6083d5802a262959483ac1059a5bcbc4dfb25b
ChronosX88 <chronosx88@gmail.com>
2019-10-24 15:34:28 +0400

    feat: [sonisd] Add Ctrl+C handler and logger

--- cmd/sonisd/main.go
+++ cmd/sonisd/main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/sirupsen/logrus"
+)
+
+var logger = logrus.New()
+
+func main() {
+	logger.Info("Starting microservice...")
+
+	logger.Info("Microservice successfully started!")
+
+	// CTRL+C handler.
+	signalHandler := make(chan os.Signal, 1)
+	shutdownDone := make(chan bool, 1)
+	signal.Notify(signalHandler, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-signalHandler
+		logger.Info("CTRL+C or SIGTERM received, shutting down openkeepd...")
+		// TODO make graceful shutdown of microservice
+		shutdownDone <- true
+	}()
+
+	<-shutdownDone
+	logger.Info("Microservice successfully shutted down")
+	os.Exit(0)
 }
