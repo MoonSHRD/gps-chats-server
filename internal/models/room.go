dc1053d8e37a14fe603883002fda745e4fbff7f2
ChronosX88 <chronosx88@gmail.com>
2019-10-26 23:26:38 +0400

    fix: [Models] Add json annotations to Room model

--- internal/models/room.go
+++ internal/models/room.go
@@ -1,9 +1,9 @@
 package models
 
 type Room struct {
-	ID        int64
-	Latitude  float32
-	Longitude float32
-	TTL       int
-	RoomID    string
+	ID        int64   `json:"id"`
+	Latitude  float32 `json:"latitude"`
+	Longitude float32 `json:"longtitude"`
+	TTL       int     `json:"ttl"`
+	RoomID    string  `json:"roomId"`
 }
